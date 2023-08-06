7a8a356f21147c1edd0f6a9ceb981415c7e76354
try-not-to-laugh <kotesin@mail.ru>
2023-08-06 20:50:23 +0500

    Task from Lesson1

---
+++ Lesson 1/Lesson1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Reflection;
+
+Assembly asm = Assembly.LoadFrom("LibraryClass.dll");
+
+//Type t = asm.GetType("Class1"); почему не работает? пишет не возвращено значений
+
+Type[] types = asm.GetTypes();
+Type type = asm.GetType(types[0].ToString());
+
+MethodInfo mi = type.GetMethod("test", BindingFlags.NonPublic | BindingFlags.Static);
+object result = mi.Invoke(null, new object[] { 2 });
+Console.WriteLine(result); 
+
