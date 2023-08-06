4bfcd14809bbc14c152811fcc59620b72e55b103
try-not-to-laugh <kotesin@mail.ru>
2023-08-06 23:55:30 +0500

    Subtask 4 from lesson 3

---
+++ Lesson 3/Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿while (true)
+{
+    Console.WriteLine("Введите температуру: ");
+    string userInput = Console.ReadLine();
+    int temperature;
+    if (int.TryParse(userInput, out temperature))
+    {
+        if (temperature > -5)
+        {
+            Console.WriteLine("Тепло");
+        }
+        else if (temperature <= -5 && temperature > -20)
+        {
+            Console.WriteLine("Нормально");
+        }
+        else if (temperature <= -20)
+        {
+            Console.WriteLine("Холодно");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели не целое число. Повторите ввод");
+    }
+
+}
+
+
+//каждое подзадание в виде отдельного коммита с соотв комментом
