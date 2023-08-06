f4523dd18f888b92a98ef4c570fc8e8008b8a606
try-not-to-laugh <kotesin@mail.ru>
2023-08-06 23:54:45 +0500

    Subtask 3 from lesson 3

---
+++ Lesson 3/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿while (true)
+{
+    Console.WriteLine("Введите целое число: ");
+    string userInput = Console.ReadLine();
+    int number;
+    if (int.TryParse(userInput, out number))
+    {
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("Четное");
+        }
+        else
+        {
+            Console.WriteLine("Нечетное");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели не целое число. Повторите ввод");
+    }
+}
