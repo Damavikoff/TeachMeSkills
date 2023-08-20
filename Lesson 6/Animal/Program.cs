eaba613d3d06f1ebdf257e06c9e7d192650dcfbd
try-not-to-laugh <kotesin@mail.ru>
2023-08-20 19:30:40 +0500

    Task from lesson 6

---
+++ Lesson 6/Animal/Program.cs
@@ -0,0 +1,32 @@
+﻿using AnimalLibrary;
+
+var cat = new Cat { Age = 3, Colour = "grey", Name = "Syuzan" };
+var dog = new Dog { Age = 5, Colour = "brown", Name = "Jacob" };
+var cow = new Cow { Age = 6, Colour = "black and white", Name = "Michel" };
+var bird = new Bird { Age = 2, Colour = "red", Name = "Peep" };
+var kitten = new Kitten { Age = 1, Colour = "orange", Name = "Mike" };
+
+kitten.MakeSound();
+kitten.MakeAction(cat);
+
+dog.MakeSound();
+cow.MakeSound();
+cat.MakeSound();
+bird.MakeSound();
+
+cat.MakeAction(dog);
+cow.MakeAction(bird);
+bird.MakeAction(cat);
+dog.MakeAction(cow);
+
+cow.Print();
+bird.Print();
+kitten.Print();
+
+cat.Print();
+cat.AddAge(1);
+cat.Print();
+kitten.Print();
+kitten.AddAge(2);
+kitten.Print();
+cat.Print();
---
+++ Lesson 6/AnimalLibrary/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+namespace AnimalLibrary
+{
+    public abstract class Animal
+    {
+        public void MakeSound()
+        {
+            Console.WriteLine($"{Name} the {GetType().Name} make {Sound}");
+        }
+        public void MakeAction(Animal class2)
+        {
+            Console.WriteLine($"{Name} the {GetType().Name} {Action} at the {class2.Name} the {class2.GetType().Name}");
+        }
+        public void Print() => Console.WriteLine($"Имя: {Name} Возраст: {Age} Цвет: {Colour} Вид: {GetType().Name}");
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Colour { get; set; }
+        public abstract string Sound { get; }
+        public abstract string Action { get; }
+    }
+}
